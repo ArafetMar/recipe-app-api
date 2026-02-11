e817ad879439ce543d4c59ecae2f6dc5417402d6
Arafet <arafet.marnissi.ia@outlook.com>
2026-02-11 11:23:09 +0100

    update Dockerfile, add database

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+calculator function
+"""
+def add(x, y):
+    """ add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """subtract x from y and return result."""
+    return y - x
