5d6e27c1f7f0b3fa8a3fdd34cab35c3accd0218f
Arafet <arafet.marnissi.ia@outlook.com>
2026-02-11 15:03:19 +0100

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,11 @@
 """
 calculator function
 """
+
 def add(x, y):
     """ add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """subtract x from y and return result."""
